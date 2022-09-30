46e4b79edf4a0ce960bbdf6d60c90fa8d724b646
Izabela88 <izabkru88@gmail.com>
2022-09-30 15:06:21 +0100

    Add intro animation

---
+++ assets/js/intro.js
@@ -0,0 +1,49 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".intro-img", { y: "0%", duration: 2 });
+tl.to(".text", { y: "0%", duration: 2 }, "-=0.75");
+tl.to(".intro", { y: "0%", duration: 1.5, delay: 1, opacity: 0 });
+
+tl.fromTo(
+  ".hero-img-two",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.25, delay: 0.1 }
+);
+tl.fromTo(
+  "#menuTogglerLabel",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.25, delay: 0.11 }
+);
+tl.fromTo(".icon", { opacity: 0 }, { opacity: 1, duration: 0.25, delay: 0.12 });
+tl.fromTo(
+  ".hero-img",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.25, delay: 0.13 }
+);
+tl.fromTo(
+  ".soc-link",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.25, stagger: 0.14 }
+);
+
+tl.fromTo(".logo", { opacity: 0 }, { opacity: 1, duration: 0.35, delay: 0.15 });
+tl.fromTo(
+  ".main-paragraph-one",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.75, delay: 0.4 }
+);
+tl.fromTo(
+  ".main-paragraph-two",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.75, delay: 0.42 }
+);
+tl.fromTo(
+  ".link-one",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.25, delay: 0.43 }
+);
+tl.fromTo(
+  ".link-two",
+  { opacity: 0 },
+  { opacity: 1, duration: 0.25, delay: 0.44 }
+);
